e5e67b045274267bcad0c8caec0621f74036f89a
systembug <albert_lee@hotmai.com>
2019-03-25 10:14:14 -0700

    no message

--- tests/unit/mixin/hotkeys-bindings-test.js
+++ tests/unit/mixin/hotkeys-bindings-test.js
@@ -2,84 +2,74 @@ import { run } from '@ember/runloop';
 import Application from '@ember/application';
 //import HotkeysBindingsMixin from "ember-tree-utils/mixins/hotkeys-bindings";
 import { module, test } from 'qunit';
-/*
-let trigger = function(type, keyCode, someKey) {
-  var e = Ember.$.Event(type);
-  e.which = keyCode;
 
-  if (someKey) {
-    e[someKey + "Key"] = true;
-  }
-  return e;
-};
-*/
-
-module('Testing Hotkeys Bindings Mixin', {
-  beforeEach() {
+module('Testing Hotkeys Bindings Mixin', function(hooks) {
+  hooks.beforeEach(function() {
     run(function() {
       //without initializing app events won't be fired when triggering events on views such as
       //keypress
       let App = Application.create();
       App.injectTestHelpers();
     });
-  },
-});
-test('basic', function(assert) {
-  assert.equal(1, 1);
-  /*
-  let TextComponent = Ember.Component.extend(HotkeysBindingsMixin, {
   });
-  let comp = TextComponent.extend({
-      hotkeysBindings: ['ctrl-a'],
 
-      actions: {
-        'ctrl+a': function() {
-          this.set('last', 'ctrl+a');
-        },
+  test('basic', function(assert) {
+    assert.equal(1, 1);
+    /*
+    let TextComponent = Ember.Component.extend(HotkeysBindingsMixin, {
+    });
+    let comp = TextComponent.extend({
+        hotkeysBindings: ['ctrl-a'],
 
-        'shift+r': function() {
-          this.set('last', 'shift+r');
-        },
+        actions: {
+          'ctrl+a': function() {
+            this.set('last', 'ctrl+a');
+          },
 
-        'alt+f10': function() {
-          this.set('last', 'alt+f10');
-        },
+          'shift+r': function() {
+            this.set('last', 'shift+r');
+          },
 
-        'alt+b': function() {
-          this.set('last', 'alt+b');
-        },
+          'alt+f10': function() {
+            this.set('last', 'alt+f10');
+          },
 
-        'meta+f12': function() {
-          this.set('last', 'meta+f12');
+          'alt+b': function() {
+            this.set('last', 'alt+b');
+          },
+
+          'meta+f12': function() {
+            this.set('last', 'meta+f12');
+          }
         }
-      }
 
+      });
+
+    let view = comp.create();
+
+    Ember.run(function() {
+      view.append();
     });
 
-  let view = comp.create();
+    let e = trigger("keypress", 65, "ctrl");
+    view.$().trigger(e);
+    assert.equal(view.get('last'), "ctrl+a");
 
-  Ember.run(function() {
-    view.append();
+    e = trigger("keypress", 82, "shift");
+    view.$().trigger(e);
+    assert.equal(view.get('last'), "shift+r");
+
+    e = trigger("keypress", 121, "alt");
+    view.$().trigger(e);
+    assert.equal(view.get('last'), 'alt+f10');
+
+    e = trigger("keydown", 66, "alt");
+    view.$().trigger(e);
+    assert.equal(view.get('last'), 'alt+b');
+
+    e = trigger("keyup", 123, "meta");
+    view.$().trigger(e);
+    assert.equal(view.get('last'), "meta+f12");
+    */
   });
-
-  let e = trigger("keypress", 65, "ctrl");
-  view.$().trigger(e);
-  assert.equal(view.get('last'), "ctrl+a");
-
-  e = trigger("keypress", 82, "shift");
-  view.$().trigger(e);
-  assert.equal(view.get('last'), "shift+r");
-
-  e = trigger("keypress", 121, "alt");
-  view.$().trigger(e);
-  assert.equal(view.get('last'), 'alt+f10');
-
-  e = trigger("keydown", 66, "alt");
-  view.$().trigger(e);
-  assert.equal(view.get('last'), 'alt+b');
-
-  e = trigger("keyup", 123, "meta");
-  view.$().trigger(e);
-  assert.equal(view.get('last'), "meta+f12");
-  */
 });
--- tests/unit/mixin/style-bindings-test.js
+++ tests/unit/mixin/style-bindings-test.js
@@ -1,28 +1,25 @@
 //import Ember from "ember";
 import { module, test } from 'qunit';
-//import StyleBindingsMixin from "ember-tree-utils/mixins/style-bindings";
 
-module('Testing Style Bindings Mixin', {
-});
+module('Testing Style Bindings Mixin', function() {
+  test('basic', function(assert) {
+    assert.equal(1, 1);
+    /*
+    let TextComponent = Ember.Component.extend(StyleBindingsMixin, {
+    });
+    let view = TextComponent.create({
+        styleBindings: ['color', 'width', 'height'],
+        color: 'blue',
+        width: 400,
+        height: '100',
+    });
 
+    Ember.run(function() {
+        view.append();
+    });
 
-test('basic', function(assert) {
-  assert.equal(1, 1);
-  /*
-  let TextComponent = Ember.Component.extend(StyleBindingsMixin, {
+    assert.equal(view.$().attr('style'), 'color:blue;width:400px;height:100;', 'Style property should be rendered properly.');
+    */
   });
-  let view = TextComponent.create({
-      styleBindings: ['color', 'width', 'height'],
-      color: 'blue',
-      width: 400,
-      height: '100',
-  });
-
-  Ember.run(function() {
-      view.append();
-  });
-
-  assert.equal(view.$().attr('style'), 'color:blue;width:400px;height:100;', 'Style property should be rendered properly.');
-  */
 });
 
