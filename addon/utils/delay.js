939d5efda1f4c246e8856cb991090cb4df2c77ec
Albert Li <albert.li@salesforce.com>
2021-11-02 06:53:23 -0700

    initial commit

--- addon/utils/delay.js
+++ addon/utils/delay.js
@@ -1,11 +1,9 @@
-import { Promise } from 'rsvp';
+import { Promise } from "rsvp";
 import { later } from "@ember/runloop";
 
-var delay = function(ms) {
+export default function delay(ms) {
   ms = ms || 1500;
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     later(this, resolve, ms);
   });
-};
-
-export default delay;
+}
